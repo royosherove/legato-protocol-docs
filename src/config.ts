2a1b28e6267fd68ead8eadce84bd17cab395d9c0
Roy <roy@osherove.com>
2022-09-10 00:33:52 +0300

    github location

--- src/config.ts
+++ src/config.ts
@@ -30,7 +30,8 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/royosherove/legato-protocol-docs/`;
+//https://github.com/royosherove/legato-protocol-docs/blob/master/src/pages//src/pages/en/introduction.md
+export const GITHUB_EDIT_URL = `https://github.com/royosherove/legato-protocol-docs/blob/master`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
